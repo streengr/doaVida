f688b5f53e1d7897744f8b86ac7325d8aa0637f9
Miguel Pereira de Oliveira <101270620+streengr@users.noreply.github.com>
2024-05-11 14:00:29 -0300

    Add files via upload

---
+++ src/main/java/com/doavida/doavida/DoavidaApplication.java
@@ -0,0 +1,13 @@
+package com.doavida.doavida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoavidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoavidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doavida/doavida/DoavidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doavida.doavida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoavidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
